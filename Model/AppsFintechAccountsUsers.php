6079503010f88c4fc9370662d572913c52eead3d
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-03-11 20:44:26 +1100

    update schema

--- Model/AppsFintechAccountsUsers.php
+++ Model/AppsFintechAccountsUsers.php
@@ -6,9 +6,15 @@ use System\Base\BaseModel;
 
 class AppsFintechAccountsUsers extends BaseModel
 {
+    protected $modelRelations = [];
+
     public $id;
+
+    public $account_id;
 
     public $first_name;
 
     public $last_name;
+
+    public $equity_balance;
 }
