f0bc3d95f46c91c82963a7c0619a1d8bbd18e21e
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2025-03-16 04:31:33 +1100

    update

--- Model/AppsFintechAccountsUsers.php
+++ Model/AppsFintechAccountsUsers.php
@@ -2,13 +2,10 @@
 
 namespace Apps\Fintech\Packages\Accounts\Users\Model;
 
-// use Apps\Fintech\Packages\Accounts\Balances\Model\AppsFintechAccountsBalances;
 use System\Base\BaseModel;
 
 class AppsFintechAccountsUsers extends BaseModel
 {
-    protected $modelRelations = [];
-
     public $id;
 
     public $account_id;
@@ -17,28 +14,4 @@ class AppsFintechAccountsUsers extends BaseModel
 
     public $last_name;
 
-    public $equity_balance;
-
-    public function initialize()
-    {
-        // $this->modelRelations['balances']['relationObj'] = $this->hasMany(
-        //     'id',
-        //     AppsFintechAccountsBalances::class,
-        //     'user_id',
-        //     [
-        //         'alias'         => 'balances'
-        //     ]
-        // );
-
-        parent::initialize();
-    }
-
-    public function getModelRelations()
-    {
-        if (count($this->modelRelations) === 0) {
-            $this->initialize();
-        }
-
-        return $this->modelRelations;
-    }
 }
